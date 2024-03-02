447673675731a450c597234da8281f4701124634
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-02 00:49:16 -0500

    0, 1, 2, 3, 4, 5, 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class Square extends Square {
+  constructor(size) {
+    super(size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
