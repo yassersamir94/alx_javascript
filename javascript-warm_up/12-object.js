0c697b299d016c7d4575c8b8bdab4ee513641b9a
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-02 00:11:16 -0500

    3

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
+
 const myObject = {
     type: 'object',
     value: 12
   };
   
-  myObject.value = 89; // Update the value property
+  // Update the value property of myObject
+  myObject.value = 89;
   
-  console.log(myObject); // Output the updated object
+  console.log(myObject);
   console.log(myObject);
   
