277e5f25bb7d3f120a99e741af58a66d0bad6f6b
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-02 23:26:27 -0500

    2

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,21 +1,21 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+const wedgeAntillesId = 18;
 
 request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
     return;
   }
-  
+
   const films = JSON.parse(body).results;
   let count = 0;
 
   // Iterate through each film
   films.forEach(film => {
     // Check if the characters array includes the URL of Wedge Antilles
-    if (film.characters.includes(wedgeAntillesUrl)) {
+    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
       count++;
     }
   });
