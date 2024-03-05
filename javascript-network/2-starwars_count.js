1f10b197bd9f30eeba434f405e5fb2bcf0059645
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-05 01:07:04 -0500

    2

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,17 +1,21 @@
 // File: 2-starwars_count.js
-const request = require('request');
+const request = require('request')
+const apiUrl = process.argv[2]
+const characterId = 18
+let count = 0
 
-const apiUrl = process.argv[2];
-const characterId = 18;
-let count = 0; // Define count variable once
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  
-  const films = JSON.parse(body).results;
-  count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length; // Update count variable
-  console.log(count);
-});
+request.get(apiUrl, function (error, response, body) {
+    if (error) {
+        console.error(error.message)
+    } else {
+        const movieData = JSON.parse(body)
+        movieData.results.forEach(function (movie) {
+            movie.characters.forEach(function (character) {
+                if (character.includes(characterId)) {
+                    count += 1
+                }
+            })
+        })
+        console.log(count)
+    }
+})
