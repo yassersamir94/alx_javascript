69b69d1173760a5fbd2e2fefa83fbc982cdd2705
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-05 01:08:46 -0500

    3

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,21 @@
+// File: 3-request_store.js
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`File ${filePath} has been saved.`);
+  });
+});
