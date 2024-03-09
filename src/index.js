1e8a40fb0b12826e5e7b3d2d37be945a5dfec41a
Yasser Samir <yassersamir.94@hotmail.com>
2024-03-08 19:05:27 -0500

    0

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function setCookies() {
+    var firstnameValue = document.getElementById('firstname').value;
+    var emailValue = document.getElementById('email').value;
+
+    document.cookie = "firstname=" + firstnameValue;
+    document.cookie = "email=" + emailValue;
+}
+
+function showCookies() {
+    var cookiesParagraph = document.createElement('p');
+    cookiesParagraph.innerHTML = "Cookies: " + document.cookie;
+    document.body.appendChild(cookiesParagraph);
+}
